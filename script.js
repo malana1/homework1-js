420a7df73e0000056b101f27280a71f40a434418
tiko malania <124159302+malana1@users.noreply.github.com>
2023-02-28 12:48:58 +0400

    Add files via upload
    first homework js

---
+++ script.js
@@ -0,0 +1,33 @@
+let number=100;
+if (number<100) {
+    console.log("naklebia50ze");
+    if(number>20){
+        console.log("metia20ze");
+    }
+ 
+} else {
+    console.log("error");
+    
+}
+    
+let x=mariami;
+if (x=mariami) {
+    console.log("true")
+    
+} else {
+    console.log("false")
+    
+}
+let username="mariami";
+switch(username){
+    case "mariami":
+    console.log("true");
+    default:
+        console.log("false");
+}
+
+
+
+
+
+
